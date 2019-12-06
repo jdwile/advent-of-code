44c9b96c57da12ab32645a0c3120cf1bed080e71
jdwile <jdwile@sep.com>
2019-12-06 11:12:36 -0500

    Day 6 solution

---
+++ 2019/6/day6.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/jdwile/advent-of-code/2019/utils"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	nodes := readInput()
+	solvePartOne(nodes)
+	solvePartTwo(nodes)
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+func readInput() map[string][]string {
+	file, _ := os.Open("./input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	nodes := make(map[string][]string)
+	for scanner.Scan() {
+		a := strings.Split(scanner.Text(), ")")
+		if nodes[a[0]] != nil {
+			if !contains(nodes[a[0]], a[1]) {
+				nodes[a[0]] = append(nodes[a[0]], a[1])
+			}
+		} else {
+			nodes[a[0]] = []string{a[1]}
+		}
+
+		if nodes[a[1]] != nil {
+			if !contains(nodes[a[1]], a[0]) {
+				nodes[a[1]] = append(nodes[a[1]], a[0])
+			}
+		} else {
+			nodes[a[1]] = []string{a[0]}
+		}
+	}
+
+	// for key, val := range nodes {
+	// 	fmt.Println(key, ":", val)
+	// }
+
+	return nodes
+}
+
+func CountIndirect(parent string, nodes map[string][]string, vi map[string]bool) int {
+	c := 0
+	var l []string
+	v := make(map[string]bool)
+	for key, val := range vi {
+		v[key] = val
+	}
+
+	if nodes[parent] == nil {
+		return c
+	}
+
+	for _, n := range nodes[parent] {
+		if !v[n] {
+			l = append(l, n)
+		}
+	}
+
+	for len(l) > 0 {
+		j := l[0]
+		l = l[1:]
+		v[j] = true
+
+		for _, n := range nodes[j] {
+			if !v[n] {
+				c += 1
+				l = append(l, n)
+			}
+		}
+	}
+
+	return c
+}
+
+func MinDistance(s, e string, nodes map[string][]string) int {
+	v := make(map[string]bool)
+	d := make(map[string]int)
+
+	l := []string{s}
+	v[s] = true
+	d[s] = 0
+
+	for len(l) > 0 {
+		c := l[0]
+		l = l[1:]
+
+		for _, k := range nodes[c] {
+			if v[k] {
+				continue
+			}
+
+			d[k] = d[c] + 1
+			l = append(l, k)
+			v[k] = true
+		}
+	}
+	return d[e]
+}
+
+func solvePartOne(nodes map[string][]string) {
+	defer utils.TimeTrack(time.Now(), "Day 6: Part 1")
+	numDirect := 1
+	numIndirect := 0
+	v := make(map[string]bool)
+	l := []string{"COM"}
+
+	for len(l) > 0 {
+		j := l[0]
+		l = l[1:]
+		v[j] = true
+
+		if nodes[j] != nil {
+			numDirect += len(nodes[j]) - 1
+			// fmt.Println(j, nodes[j], numDirect)
+			numIndirect += CountIndirect(j, nodes, v)
+
+			for _, k := range nodes[j] {
+				if !v[k] {
+					l = append(l, k)
+				}
+			}
+		}
+	}
+
+	fmt.Println("Part 1:", numDirect + numIndirect)
+}
+
+func solvePartTwo(nodes map[string][]string) {
+	defer utils.TimeTrack(time.Now(), "Day 6: Part 2")
+	fmt.Println("Part 2:", MinDistance("YOU", "SAN", nodes) - 2)
+}

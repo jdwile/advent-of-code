9288a55d4e3a9014ffff3b5ba14f135f5d6df4d2
Jenner Wile <jdwile@sep.com>
2020-01-20 09:49:26 -0500

    Move common functions/structs to utils

--- 2017/utils/utils.go
+++ 2017/utils/utils.go
@@ -6,9 +6,25 @@ import (
 	"time"
 )
 
-func TimeTrack(start time.Time, name string) {
-	elapsed := time.Since(start)
-	fmt.Printf("%s took %s\n", name, elapsed)
+func Abs(a int) int {
+	if a < 0 {
+		return -1 * a
+	}
+	return a
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func Mute() func() {
@@ -22,4 +38,14 @@ func Mute() func() {
 		os.Stdout = sout
 		os.Stderr = serr
 	}
+}
+
+func TimeTrack(start time.Time, name string) {
+	elapsed := time.Since(start)
+	fmt.Printf("%s took %s\n", name, elapsed)
+}
+
+type Point struct {
+	X int
+	Y int
 }

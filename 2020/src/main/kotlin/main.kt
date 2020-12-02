8c919dda209bce9d04641a66c7fc97c37ac34864
jdwile <jdwile@sep.com>
2020-12-02 09:32:31 -0500

    Day 2 complete

--- 2020/src/main/kotlin/main.kt
+++ 2020/src/main/kotlin/main.kt
@@ -7,8 +7,10 @@ fun main() {
    solutions.add(Day1())
    solutions.add(Day2())
 
-   solutions.forEach {
-      println(it.part1())
-      println(it.part2())
-   }
+//   solutions.forEach {
+//      println(it.part1())
+//      println(it.part2())
+//   }
+   println(solutions[2].part1())
+   println(solutions[2].part2())
 }
--- 2020/src/main/kotlin/problems/Day2.kt
+++ 2020/src/main/kotlin/problems/Day2.kt
@@ -4,10 +4,42 @@ import common.*
 
 class Day2: ISolution {
     override fun part1(): String {
-        return "Day 2, Part 1: "
+        val lines = readFile("${getPath()}/problems/input/2.in")
+        var total = 0
+
+        lines.forEach {
+            val entries = it.split("([ \\-:])+".toRegex())
+            val (low, high, letter, password) = entries.toCollection(ArrayList())
+
+            if (isValidPassword1(low.toInt(), high.toInt(), letter[0], password)) {
+                total++
+            }
+        }
+
+        return "Day 2, Part 1: $total"
     }
 
     override fun part2(): String {
-        return "Day 2, Part 2: "
+        val lines = readFile("${getPath()}/problems/input/2.in")
+        var total = 0
+
+        lines.forEach {
+            val entries = it.split("([ \\-:])+".toRegex())
+            val (low, high, letter, password) = entries.toCollection(ArrayList())
+
+            if (isValidPassword2(low.toInt(), high.toInt(), letter[0], password)) {
+                total++
+            }
+        }
+
+        return "Day 2, Part 2: $total"
+    }
+
+    private fun isValidPassword1(low: Int, high: Int, letter: Char, password: String): Boolean {
+        return password.count { it == letter } in low..high
+    }
+
+    private fun isValidPassword2(low: Int, high: Int, letter: Char, password: String): Boolean {
+        return (password[low-1] == letter).xor(password[high-1] == letter)
     }
 }

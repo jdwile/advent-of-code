a609c4ba4165d85c619d0bf579379884db1447b6
jdwile <jdwile@sep.com>
2020-12-02 09:39:56 -0500

    Add timing + Day 3 scaffold

--- 2020/src/main/kotlin/main.kt
+++ 2020/src/main/kotlin/main.kt
@@ -1,16 +1,26 @@
 import common.ISolution
 import problems.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
    val solutions = ArrayList<ISolution>()
    solutions.add(Example())
    solutions.add(Day1())
    solutions.add(Day2())
+   solutions.add(Day3())
 
-//   solutions.forEach {
-//      println(it.part1())
-//      println(it.part2())
-//   }
-   println(solutions[2].part1())
-   println(solutions[2].part2())
+   solutions.forEach {
+      val part1Time = measureTimeMillis {
+         print(it.part1())
+      }
+      println(" - $part1Time ms")
+
+      val part2Time = measureTimeMillis {
+         print(it.part2())
+      }
+      println(" - $part2Time ms")
+   }
+
+//   println(solutions[3].part1())
+//   println(solutions[3].part2())
 }
---
+++ 2020/src/main/kotlin/problems/Day3.kt
@@ -0,0 +1,13 @@
+package problems
+
+import common.*
+
+class Day3: ISolution {
+    override fun part1(): String {
+        return "Day 3, Part 1: "
+    }
+
+    override fun part2(): String {
+        return "Day 3, Part 2: "
+    }
+}

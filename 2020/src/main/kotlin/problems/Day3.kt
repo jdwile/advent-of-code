0bdf43a159c57fb1584a7cd85eb7b8710b6a19b5
jdwile <jdwile@sep.com>
2020-12-03 10:21:12 -0500

    Day 3 solution

--- 2020/src/main/kotlin/problems/Day3.kt
+++ 2020/src/main/kotlin/problems/Day3.kt
@@ -1,13 +1,44 @@
 package problems
 
 import common.*
+import java.math.BigInteger
 
 class Day3: ISolution {
     override fun part1(): String {
-        return "Day 3, Part 1: "
+        val grid = readFile("${getPath()}/problems/input/3.in")
+
+        val treeCount = getTreeCount(grid)
+
+        return "Day 3, Part 1: $treeCount"
     }
 
     override fun part2(): String {
-        return "Day 3, Part 2: "
+        val grid = readFile("${getPath()}/problems/input/3.in")
+
+        var treeCountProduct: BigInteger = BigInteger.ONE
+
+        treeCountProduct *= getTreeCount(grid, 1, 1).toBigInteger()
+        treeCountProduct *= getTreeCount(grid, 3, 1).toBigInteger()
+        treeCountProduct *= getTreeCount(grid, 5, 1).toBigInteger()
+        treeCountProduct *= getTreeCount(grid, 7, 1).toBigInteger()
+        treeCountProduct *= getTreeCount(grid, 1, 2).toBigInteger()
+
+        return "Day 3, Part 2: $treeCountProduct"
+    }
+
+    private fun getTreeCount(grid: ArrayList<String>, dx: Int = 3, dy: Int = 1): Int {
+        var x = 0
+        var y = 0
+        var treeCount = 0
+
+        while (y < grid.size) {
+            if (grid[y][x] == '#') {
+                treeCount++
+            }
+            x = (x + dx) % grid[y].length
+            y += dy
+        }
+
+        return treeCount
     }
 }

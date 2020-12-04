64c9fdf15a746c6e1c5122157aa5592a0775c37b
jdwile <jdwile@sep.com>
2020-12-04 07:41:27 -0500

    Day 4 complete

--- 2020/src/main/kotlin/problems/Day4.kt
+++ 2020/src/main/kotlin/problems/Day4.kt
@@ -1,13 +1,63 @@
 package problems
 
 import common.*
+import java.io.File
 
 class Day4: ISolution {
     override fun part1(): String {
-        return "Day 4, Part 1: "
+        val passports = readPassports()
+
+        val res = passports.count { reqFieldsPresent(it) }
+
+        return "Day 4, Part 1: $res"
     }
 
     override fun part2(): String {
-        return "Day 4, Part 2: "
+        val passports = readPassports()
+
+        val res = passports.count { reqFieldsValid(it) }
+
+        return "Day 4, Part 2: $res"
+    }
+
+    private fun readPassports(): ArrayList<HashMap<String, String>> {
+        var res = ArrayList<HashMap<String, String>>()
+        var passport = HashMap<String, String>()
+
+        File("${getPath()}/problems/input/4.in").forEachLine {
+            if (it.isEmpty()) {
+                res.add(passport)
+                passport = HashMap()
+            } else {
+                val entries = it.split(" ")
+                for (entry in entries) {
+                    val kvp = entry.split(":")
+                    passport[kvp[0]] = kvp[1]
+                }
+            }
+        }
+        return res
+    }
+
+    private fun reqFieldsPresent(passport: HashMap<String, String>): Boolean {
+        return !passport["byr"].isNullOrBlank()
+                && !passport["iyr"].isNullOrBlank()
+                && !passport["eyr"].isNullOrBlank()
+                && !passport["hgt"].isNullOrBlank()
+                && !passport["hcl"].isNullOrBlank()
+                && !passport["ecl"].isNullOrBlank()
+                && !passport["pid"].isNullOrBlank()
+    }
+
+    private fun reqFieldsValid(passport: HashMap<String, String>): Boolean {
+        return reqFieldsPresent(passport)
+                && passport["byr"]?.toInt() in 1920..2002
+                && passport["iyr"]?.toInt() in 2010..2020
+                && passport["eyr"]?.toInt() in 2020..2030
+                && (passport["hgt"]!!.matches("^1([5-8][0-9]|9[0-3])cm$".toRegex())
+                    || passport["hgt"]!!.matches("^(59|6[0-9]|7[0-6])in$".toRegex()))
+                && passport["hcl"]!!.matches("^#([0-9]|[a-f]){6}$".toRegex())
+                && passport["ecl"] in arrayOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth")
+                && passport["pid"]!!.matches("^([0-9]){9}$".toRegex())
     }
 }

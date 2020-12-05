a6e7a8d280839049a635c7166f53f2076405f6d2
jdwile <jdwile@sep.com>
2020-12-05 08:53:46 -0500

    Day 5 solution

--- 2020/src/main/kotlin/problems/Day5.kt
+++ 2020/src/main/kotlin/problems/Day5.kt
@@ -4,10 +4,30 @@ import common.*
 
 class Day5: ISolution {
     override fun part1(): String {
-        return "Day 5, Part 1"
+        val barcodes = readFileAsStrings("${getPath()}/problems/input/5.in")
+
+        val max = barcodes.map(::getSeatId).maxOrNull()!!
+
+        return "Day 5, Part 1: $max"
     }
 
     override fun part2(): String {
-        return "Day 5, Part 2"
+        val barcodes = readFileAsStrings("${getPath()}/problems/input/5.in")
+
+        val myId = barcodes
+                .map(::getSeatId)
+                .sorted()
+                .zipWithNext()
+                .first { it.second - it.first == 2 }
+                .first + 1
+
+        return "Day 5, Part 2: $myId"
+    }
+
+    private fun getSeatId(barcode: String): Int {
+        return barcode
+                .replace(Regex("[BR]"), "1")
+                .replace(Regex("[FL]"), "0")
+                .toInt(2)
     }
 }

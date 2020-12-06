1e77888d09bc46803c55b007b1fe2b3ed154a9c5
jdwile <jdwile@sep.com>
2020-12-06 08:28:23 -0500

    Day 6 solution

--- 2020/src/main/kotlin/main.kt
+++ 2020/src/main/kotlin/main.kt
@@ -2,9 +2,6 @@ import common.ISolution
 import problems.*
 import java.math.RoundingMode
 import java.text.DecimalFormat
-import java.text.NumberFormat
-import javax.swing.text.NumberFormatter
-import kotlin.system.measureNanoTime
 import kotlin.system.measureTimeMillis
 
 fun main() {
@@ -20,22 +17,22 @@ fun main() {
    dec.roundingMode = RoundingMode.HALF_UP
 
    val time = measureTimeMillis {
-      solutions.forEach {
-         val part1Time: Double = measureNanoTime {
-            repeat(5) { _ -> it.part1() }
-         }.toDouble()
-         print(it.part1())
-         println(" - " + dec.format(part1Time / 5 / 1000000) + "ms")
+//      solutions.forEach {
+//         val part1Time: Double = measureNanoTime {
+//            repeat(5) { _ -> it.part1() }
+//         }.toDouble()
+//         print(it.part1())
+//         println(" - " + dec.format(part1Time / 5 / 1000000) + "ms")
+//
+//         val part2Time: Double = measureNanoTime {
+//            repeat(5) { _ -> it.part2() }
+//         }.toDouble()
+//         print(it.part2())
+//         println(" - " + dec.format(part2Time / 5 / 1000000) + "ms")
+//      }
 
-         val part2Time: Double = measureNanoTime {
-            repeat(5) { _ -> it.part2() }
-         }.toDouble()
-         print(it.part2())
-         println(" - " + dec.format(part2Time / 5 / 1000000) + "ms")
-      }
-
-//      println(solutions[6].part1())
-//      println(solutions[6].part2())
+      println(solutions[5].part1())
+      println(solutions[5].part2())
    }
    println(" --- $time ms total")
 }
--- 2020/src/main/kotlin/problems/Day6.kt
+++ 2020/src/main/kotlin/problems/Day6.kt
@@ -4,10 +4,49 @@ import common.*
 
 class Day6: ISolution {
     override fun part1(): String {
-        return "Day 6, Part 1"
+        val lines = readFileAsStrings("${getPath()}/problems/input/6.in")
+
+        val res = lines
+                .let(::parseGroups)
+                .map{ it.answers.entries.size }
+                .fold(0) { acc, it -> acc + it }
+
+        return "Day 6, Part 1 - $res"
     }
 
     override fun part2(): String {
-        return "Day 6, Part 2"
+        val lines = readFileAsStrings("${getPath()}/problems/input/6.in")
+
+        val res = lines
+                .let(::parseGroups)
+                .map { group -> group.answers.count{ it.value == group.size }}
+                .fold(0) { acc, it -> acc + it }
+
+        return "Day 6, Part 2 - $res"
     }
+
+    private fun parseGroups(entries: List<String>): List<Group> {
+        val groups = ArrayList<Group>()
+        var group = HashMap<String, Int>()
+        var groupSize = 0
+
+        entries.forEachIndexed { i, entry ->
+            if (entry.isNotEmpty()) {
+                groupSize++
+                entry.toCharArray()
+                        .map{ it.toString() }
+                        .groupingBy { it }.eachCount()
+                        .forEach { group.merge(it.key, it.value, Math::addExact) }
+            }
+            if (entry.isEmpty() || i == entries.lastIndex) {
+                groups.add(Group(group, groupSize))
+                groupSize = 0
+                group = HashMap()
+            }
+        }
+
+        return groups
+    }
+
+    private data class Group(val answers: Map<String, Int>, val size: Int)
 }

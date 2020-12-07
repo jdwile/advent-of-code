14ef397a4c36878ba037c2578030350b0bcc7238
jdwile <jdwile@sep.com>
2020-12-07 10:14:39 -0500

    Refactor to better use data class

--- 2020/src/main/kotlin/problems/Day7.kt
+++ 2020/src/main/kotlin/problems/Day7.kt
@@ -8,11 +8,8 @@ class Day7: ISolution {
     override fun part1(): String {
         val lines = readFileAsStrings("${getPath()}/problems/input/7.in")
 
-        val adjacencyList = getAdjacencyList(lines)
-        val bagsThatContainGold =  adjacencyList.keys
-                                    .filter { it != "shiny gold" }
-                                    .map { if (bagContainsGold(adjacencyList, it, ArrayList(), ArrayList())) 1 else 0 }
-                                    .sum()
+        val bags = getBags(lines)
+        val bagsThatContainGold = bags.filter { it.value.contains("shiny gold", bags) }.count()
 
         return "Day 7, Part 1 - $bagsThatContainGold"
     }
@@ -20,73 +17,41 @@ class Day7: ISolution {
     override fun part2(): String {
         val lines = readFileAsStrings("${getPath()}/problems/input/7.in")
 
-        val adjacencyList = getAdjacencyList(lines)
-        val totalBagsContainedInGold = getTotalBagsContainedIn(adjacencyList, "shiny gold")
+        val bags = getBags(lines)
+        val totalBagsContainedInGold = bags["shiny gold"]?.countInnerBags(bags)
+
         return "Day 7, Part 2 - $totalBagsContainedInGold"
     }
 
-    private fun getTotalBagsContainedIn(adjacencyList: Map<String, List<Bag>>, currentBag: String): Int {
-        var total = 0
+    private data class Bag(val name: String, val innerBags: Map<String, Int>) {
+        fun contains(name: String, bags: Map<String, Bag>): Boolean {
+            if (this.innerBags.keys.contains(name)) return true
 
-        adjacencyList[currentBag]?.let { subBags ->
-            subBags.forEach {
-                total += it.count * (1 + getTotalBagsContainedIn(adjacencyList, it.name))
-            }
+            return this.innerBags.keys.any { bags[it]?.contains(name, bags) == true }
         }
 
-        return total
+        fun countInnerBags(bags: Map<String, Bag>): Int {
+            return this.innerBags.entries.sumBy { (bags[it.key]?.countInnerBags(bags) ?: 0) * it.value + it.value }
+        }
     }
 
-    private tailrec fun bagContainsGold(adjacencyList: Map<String, List<Bag>>, bag: String, next: ArrayList<String>, visited: ArrayList<String>): Boolean {
-        if (bag == "shiny gold") {
-            return true
-        }
-
-        next.remove(bag)
-        visited.add(bag)
-
-        adjacencyList[bag]?.let { cur ->
-            next.addAll(cur.filter { !visited.contains(it.name) && !next.contains(it.name) }.map { it.name })
-        }
-
-        if (next.size == 0) return false
-
-        return bagContainsGold(adjacencyList, next[0], next, visited)
-    }
-
-    private data class Bag(val count: Int, val name: String)
-
-    private fun getAdjacencyList(lines: List<String>): Map<String, List<Bag>> {
-        val res = HashMap<String, ArrayList<Bag>>()
+    private fun getBags(lines: List<String>): Map<String, Bag> {
         val processedLines = ArrayList<String>()
 
-        lines.forEach {
-            processedLines.add(
-                    it.replace("contain", "-")
-                            .replace("[\\.]|\\sbags|\\sbag".toRegex(), "")
-                            .replace(",", " :")
-            )
-        }
-
-        processedLines.forEach { line ->
-            val (cur, rest) = line.split(" - ")
-
-            if (rest == "no other") {
-                res[cur] = ArrayList()
-            } else {
-                rest.split(" : ").forEach {
-                    val bagArr = it.split(" ")
-                    val count = bagArr[0].toInt()
-                    val bagName = bagArr.subList(1, bagArr.lastIndex+1).joinToString(" ")
-
-                    if (res[cur].isNullOrEmpty()) {
-                        res[cur] = ArrayList()
-                    }
-                    res[cur]?.add(Bag(count, bagName))
-                }
+        lines.filter { !it.contains("no other") }
+            .forEach {
+                processedLines.add(
+                        it.replace(" contain ", "-")
+                                .replace("[\\.]|\\sbags|\\sbag".toRegex(), "")
+                                .replace(", ", ":")
+                )
             }
-        }
 
-        return res
+        return processedLines.map { it.split("-", ":") }
+                .map { it[0] to Bag(it[0], toInnerBags(it.subList(1, it.size)))}.toMap()
     }
+
+    private fun toInnerBags(bags: List<String>) =
+            bags.map { it.substring(2) to Integer.parseInt(it.substring(0, 1)) }.toMap()
+
 }

4b803e50f685a7d936311d40b61096ba438c4578
jdwile <jdwile@sep.com>
2020-12-08 08:18:08 -0500

    Day 8 solution

--- 2020/src/main/kotlin/problems/Day8.kt
+++ 2020/src/main/kotlin/problems/Day8.kt
@@ -4,10 +4,73 @@ import common.*
 
 class Day8: ISolution {
     override fun part1(): String {
-        return "Day 8, Part 1 - "
+        val program = readFileAsStrings("${getPath()}/problems/input/8.in")
+                .map { it.split(" ") }
+                .map { Instruction(it[0], it[1].toInt()) }
+
+        val (acc, _) = runProgram(program)
+
+        return "Day 8, Part 1 - $acc"
     }
 
     override fun part2(): String {
-        return "Day 8, Part 2 - "
+        val program = readFileAsStrings("${getPath()}/problems/input/8.in")
+                .map { it.split(" ") }
+                .map { Instruction(it[0], it[1].toInt()) }
+
+        val attempts = HashSet<Int>()
+
+        while (true) {
+            var flippedIndex = 0
+            while (flippedIndex < program.size && (program[flippedIndex].operation == "acc" || flippedIndex in attempts)) flippedIndex++
+            attempts.add(flippedIndex)
+
+            val testProgram = program.map(Instruction::clone)
+            when (testProgram[flippedIndex].operation) {
+                "nop" -> testProgram[flippedIndex].operation = "jmp"
+                "jmp" -> testProgram[flippedIndex].operation = "nop"
+            }
+
+            val (acc, hasLooped) = runProgram(testProgram)
+
+            if (!hasLooped) {
+                return "Day 8, Part 2 - $acc"
+            }
+        }
+    }
+
+    class Instruction constructor(var operation: String, val arg: Int, var visited: Boolean = false) {
+        override fun toString() = "$operation: $arg (visited=$visited)"
+        fun clone(): Instruction {
+            return Instruction(operation, arg, visited)
+        }
+    }
+
+    private fun runProgram(program: List<Instruction>): Pair<Int, Boolean> {
+        var i = 0
+        var acc = 0
+
+        var hasLooped = false
+
+        while (true) {
+            if (i == program.size) break
+
+            if (program[i].visited) {
+                hasLooped = true
+                break
+            }
+
+            program[i].visited = true
+
+            when (program[i].operation) {
+                "acc" -> acc += program[i].arg
+                "jmp" -> i += program[i].arg - 1
+                "nop" -> {
+                }
+            }
+            i++
+        }
+
+        return Pair(acc, hasLooped)
     }
 }

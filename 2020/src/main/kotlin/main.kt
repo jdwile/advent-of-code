f90f23b5ead0166e603cf28f491c0cc5110a5268
jdwile <jdwile@sep.com>
2020-12-11 08:13:34 -0500

    Day 11 solution

--- 2020/src/main/kotlin/main.kt
+++ 2020/src/main/kotlin/main.kt
@@ -5,35 +5,35 @@ import java.text.DecimalFormat
 import kotlin.system.measureNanoTime
 
 fun main() {
-   val solutions = ArrayList<ISolution>()
-   solutions.add(Day1())
-   solutions.add(Day2())
-   solutions.add(Day3())
-   solutions.add(Day4())
-   solutions.add(Day5())
-   solutions.add(Day6())
-   solutions.add(Day7())
-   solutions.add(Day8())
-   solutions.add(Day9())
-   solutions.add(Day10())
-   solutions.add(Day11())
+    val solutions = ArrayList<ISolution>()
+    solutions.add(Day1())
+    solutions.add(Day2())
+    solutions.add(Day3())
+    solutions.add(Day4())
+    solutions.add(Day5())
+    solutions.add(Day6())
+    solutions.add(Day7())
+    solutions.add(Day8())
+    solutions.add(Day9())
+    solutions.add(Day10())
+    solutions.add(Day11())
 
-   val dec = DecimalFormat("###,###.##")
-   dec.roundingMode = RoundingMode.HALF_UP
+    val dec = DecimalFormat("###,###.##")
+    dec.roundingMode = RoundingMode.HALF_UP
 
-   solutions.forEach {
-      val part1Time: Double = measureNanoTime {
-         repeat(50) { _ -> it.part1() }
-      }.toDouble()
-      print(it.part1())
-      println(" - " + dec.format(part1Time / 50 / 1000000) + "ms")
+    solutions.forEach {
+        val part1Time: Double = measureNanoTime {
+            repeat(50) { _ -> it.part1() }
+        }.toDouble()
+        print(it.part1())
+        println(" - " + dec.format(part1Time / 50 / 1000000) + "ms")
 
-      val part2Time: Double = measureNanoTime {
-         repeat(50) { _ -> it.part2() }
-      }.toDouble()
-      print(it.part2())
-      println(" - " + dec.format(part2Time / 50 / 1000000) + "ms")
-   }
+        val part2Time: Double = measureNanoTime {
+            repeat(50) { _ -> it.part2() }
+        }.toDouble()
+        print(it.part2())
+        println(" - " + dec.format(part2Time / 50 / 1000000) + "ms")
+    }
 
 //      println(solutions[10].part1())
 //      println(solutions[10].part2())
--- 2020/src/main/kotlin/problems/Day11.kt
+++ 2020/src/main/kotlin/problems/Day11.kt
@@ -2,12 +2,84 @@ package problems
 
 import common.*
 
-class Day11: ISolution {
+class Day11 : ISolution {
     override fun part1(): String {
-        return "Day 11, Part 1 - "
+        var seats = readFileAsCharArray("${getPath()}/problems/input/11.in")
+        seats = runGameOfAirplaneSeats(seats)
+
+        val res = seats.hash().count { it == '#' }
+
+        return "Day 11, Part 1 - $res"
     }
 
     override fun part2(): String {
-        return "Day 11, Part 2 - "
+        var seats = readFileAsCharArray("${getPath()}/problems/input/11.in")
+        seats = runGameOfAirplaneSeats(seats, true)
+
+        val res = seats.hash().count { it == '#' }
+
+        return "Day 11, Part 2 - $res"
+    }
+
+    private fun Array<CharArray>.hash(): String {
+        return this.joinToString("\n") { it.joinToString("") }
+    }
+
+    private fun runGameOfAirplaneSeats(plane: Array<CharArray>, seesExtraFar: Boolean = false): Array<CharArray> {
+        var last: String
+        val diffs = arrayOf(-1, 0, 1)
+        var seats = plane
+
+        do {
+            last = seats.hash()
+            val next = Array(seats.size) { CharArray(seats[0].size) }
+
+            for (x in seats.indices) {
+                for (y in seats[x].indices) {
+                    next[x][y] = seats[x][y]
+
+                    if (seats[x][y] == '.') continue
+                    var numOccupiedNeighbors = 0
+
+                    for (dx in diffs) {
+                        if (x + dx !in seats.indices) continue
+                        for (dy in diffs) {
+                            if ((dx == 0 && dy == 0) || y + dy !in seats[x].indices) continue
+
+                            var multi = 1
+                            while (seesExtraFar &&
+                                    x + dx * multi in seats.indices
+                                    && y + dy * multi in seats[x].indices
+                                    && seats[x + dx * multi][y + dy * multi] == '.') {
+                                multi++
+                            }
+                            if (x + dx * multi in seats.indices
+                                    && y + dy * multi in seats[x].indices
+                                    && seats[x + dx * multi][y + dy * multi] == '#') {
+                                numOccupiedNeighbors++
+                            }
+                        }
+                    }
+                    next[x][y] = calculateSeatBehavior(seats[x][y], numOccupiedNeighbors, seesExtraFar)
+                }
+            }
+            seats = next.toList().toTypedArray()
+        } while (seats.hash() != last)
+        return seats
+    }
+
+    private fun calculateSeatBehavior(seat: Char, numOccupiedNeighbors: Int, tolerant: Boolean = false): Char {
+        if (seat == 'L' && numOccupiedNeighbors == 0) {
+            return '#'
+        }
+
+        if (seat == '#' && numOccupiedNeighbors >= 4) {
+            if (tolerant && numOccupiedNeighbors == 4) {
+                return '#'
+            }
+            return 'L'
+        }
+
+        return seat
     }
 }

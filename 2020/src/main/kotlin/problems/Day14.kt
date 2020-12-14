cb46f880b0c031dd075587ddf2724513b3464d9f
Jenner Wile <Jenner.Wile@adesa.com>
2020-12-14 07:51:18 -0500

    Day 14 solution

--- 2020/src/main/kotlin/problems/Day14.kt
+++ 2020/src/main/kotlin/problems/Day14.kt
@@ -1,13 +1,84 @@
 package problems
 
 import common.*
+import java.lang.Long.toBinaryString
 
 class Day14 : ISolution {
     override fun part1(): String {
-        return "Day 14, Part 1 - "
+        val lines = readFileAsStrings("${getPath()}/problems/input/14.in")
+        val memoryRegex = Regex("^mem\\[([0-9]*)\\] = ([0-9]*)\$")
+        val maskRegex = Regex("^mask = (.*)\$")
+        var bitmask = ""
+        val memory = HashMap<Long, Long>()
+
+        lines.forEach { line ->
+            if (maskRegex.matches(line)) {
+                bitmask = maskRegex.matchEntire(line)!!.groupValues[1]
+            } else {
+                val (memLoc, memVal) = memoryRegex.matchEntire(line)!!.groupValues.subList(1, 3).map { it.toLong() }
+
+                memory[memLoc] = memVal.overwriteWithBinaryMask(bitmask)
+            }
+        }
+
+        val res = memory.values.sum()
+
+        return "Day 14, Part 1 - $res"
     }
 
     override fun part2(): String {
-        return "Day 14, Part 2 - "
+        val lines = readFileAsStrings("${getPath()}/problems/input/14.in")
+        val memoryRegex = Regex("^mem\\[([0-9]*)\\] = ([0-9]*)\$")
+        val maskRegex = Regex("^mask = (.*)\$")
+        var bitmask = ""
+        val memory = HashMap<Long, Long>()
+
+        lines.forEach { line ->
+            if (maskRegex.matches(line)) {
+                bitmask = maskRegex.matchEntire(line)!!.groupValues[1]
+            } else {
+                val (memLoc, memVal) = memoryRegex.matchEntire(line)!!.groupValues.subList(1, 3).map { it.toLong() }
+
+                val locations = arrayListOf(memLoc.floatingBinaryMask(bitmask))
+
+                while (locations[0].contains('X')) {
+                    val cur = locations[0]
+                    locations.removeAt(0)
+                    locations.add(cur.replaceFirst('X', '0'))
+                    locations.add(cur.replaceFirst('X', '1'))
+                }
+
+                locations.map { it.toLong(2) }
+                        .map {
+                            memory[it] = memVal
+                        }
+            }
+        }
+
+        val res = memory.values.sum()
+
+        return "Day 14, Part 2 - $res"
     }
+
+    private fun Long.to36bitString(): String =
+            toBinaryString(this).padStart(36, '0')
+
+    private fun Long.overwriteWithBinaryMask(mask: String): Long =
+            this.to36bitString().mapIndexed { i, c ->
+                when (mask[i]) {
+                    '0' -> '0'
+                    '1' -> '1'
+                    else -> c
+                }
+            }.joinToString("")
+                    .toLong(2)
+
+    private fun Long.floatingBinaryMask(mask: String): String =
+            this.to36bitString().mapIndexed { i, c ->
+                when (mask[i]) {
+                    '0' -> c
+                    '1' -> '1'
+                    else -> 'X'
+                }
+            }.joinToString("")
 }

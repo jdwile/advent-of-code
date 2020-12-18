272252713144edd3844e827573b8af03772fd583
Jenner Wile <Jenner.Wile@adesa.com>
2020-12-18 08:05:35 -0500

    Day 18 solution

--- 2020/src/main/kotlin/problems/Day18.kt
+++ 2020/src/main/kotlin/problems/Day18.kt
@@ -1,13 +1,115 @@
 package main.kotlin.problems
 
 import main.kotlin.common.*
+import java.util.*
 
 class Day18 : ISolution {
+    val DIGITS = (0..9).joinToString().toCharArray()
+    lateinit var operands: Stack<Long>
+    lateinit var operators: Stack<Char>
+
     override fun part1(): String {
-        return "Day 18, Part 1 - "
+        val lines = readFileAsStrings("18.in")
+        var total: Long = 0
+
+        lines.forEach { line ->
+            operands = Stack<Long>()
+            operators = Stack<Char>()
+
+            for (i in line.indices) {
+                val cur = line[i]
+                when (cur) {
+                    ' ' -> continue
+                    in DIGITS -> {
+                        operands.push(cur.toString().toLong())
+                    }
+                    '+', '*' -> {
+                        evaluate()
+                        operators.push(cur)
+                    }
+                    '(' -> operators.push(cur)
+                    ')' -> evaluate(true)
+                    else -> throw Exception(":(")
+                }
+            }
+            evaluate()
+            total += operands.pop()
+        }
+
+        return "Day 18, Part 1 - $total"
     }
 
     override fun part2(): String {
-        return "Day 18, Part 2 - "
+        val lines = readFileAsStrings("18.in")
+        var total: Long = 0
+
+        lines.forEach { line ->
+            operands = Stack<Long>()
+            operators = Stack<Char>()
+
+            for (i in line.indices) {
+                val cur = line[i]
+                when (cur) {
+                    ' ' -> continue
+                    in DIGITS -> {
+                        operands.push(cur.toString().toLong())
+                    }
+                    '+', '*' -> {
+                        evaluateWithPrecedence(cur)
+                        operators.push(cur)
+                    }
+                    '(' -> operators.push(cur)
+                    ')' -> evaluateWithPrecedence(cur, true)
+                    else -> throw Exception(":(")
+                }
+            }
+            evaluate()
+            total += operands.pop()
+        }
+
+        return "Day 18, Part 2 - $total"
+    }
+
+    private fun evaluate(shouldRemoveParen: Boolean = false) {
+        while (!operators.isEmpty()) {
+            if (operators.peek() == '(') {
+                if (shouldRemoveParen) operators.pop()
+                break
+            }
+            val op = operators.pop()
+            val num1 = operands.pop()
+            val num2 = operands.pop()
+
+            when (op) {
+                '+' -> operands.push(num1 + num2)
+                '*' -> operands.push(num1 * num2)
+            }
+        }
+    }
+
+    private fun evaluateWithPrecedence(precedent: Char, shouldRemoveParen: Boolean = false) {
+        val allowed = when (precedent) {
+            '+' -> listOf('+')
+            '*' -> listOf('+')
+            ')' -> listOf('*', '+')
+            else -> throw Exception(":(")
+        }
+
+        while (!operators.isEmpty()) {
+            if (operators.peek() == '(') {
+                if (shouldRemoveParen) operators.pop()
+                break
+            }
+            if (operators.peek() !in allowed) break
+
+            val op = operators.pop()
+            val num1 = operands.pop()
+            val num2 = operands.pop()
+
+            when (op) {
+                '+' -> operands.push(num1 + num2)
+                '*' -> operands.push(num1 * num2)
+            }
+        }
     }
 }

a4cf4d5c537cf0be9714e56d409a23e8c64f4b7a
jdwile <jdwile@sep.com>
2020-12-19 09:47:39 -0500

    Day 19 solution

--- 2020/src/main/kotlin/problems/Day19.kt
+++ 2020/src/main/kotlin/problems/Day19.kt
@@ -1,13 +1,123 @@
 package main.kotlin.problems
 
 import main.kotlin.common.*
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class Day19 : ISolution {
+    private val digitRegex = Regex("\\d+")
+
     override fun part1(): String {
-        return "Day 19, Part 1 - "
+        val (rawRules, messages) = parseInput(readFileAsStrings("19.in"))
+
+        val rules = HashMap<Int, String>()
+
+        rawRules.forEach {
+            val groups = it.split(": ")
+            rules[groups[0].toInt()] = groups[1]
+        }
+
+        var s = rules[0]!!
+        while (digitRegex.containsMatchIn(s)) {
+            s = digitRegex.replace(s) { m -> "(${rules[m.value.toInt()]})" }
+        }
+
+        var regex = Regex("^" + s.replace(" ", "").replace("\"", "") + "\$")
+
+        var res = messages.filter { regex.matches(it) }.size
+
+        return "Day 19, Part 1 - $res"
     }
 
     override fun part2(): String {
-        return "Day 19, Part 2 - "
+        val (rawRules, messages) = parseInput(readFileAsStrings("19.in"))
+
+        val rules = rawRules.associate { s ->
+            s.split(": ").let { (a, b) ->
+                a.toInt() to (b.removeSurrounding("\"").split(" | ").map { it.split(' ') })
+            }
+        }
+
+        val res = messages.count { it.checkWith(rules).contains("") }
+
+        return "Day 19, Part 2 - $res"
+    }
+
+    private fun String.checkWith(rules: Map<Int, List<List<String>>>, cur: Int = 0): Set<String> {
+        val res = mutableSetOf<String>()
+
+        when (cur) {
+            8 -> {
+                val next = 42
+                var rest = setOf(this)
+
+                while (rest.isNotEmpty()) {
+                    rest = rest.flatMap { it.checkWith(rules, next) }.toSet()
+                    res += rest
+                }
+            }
+
+            11 -> {
+                val first = 42
+                val second = 31
+                var rest = setOf(this)
+                rest = rest.flatMap { it.checkWith(rules, first) }.toSet()
+
+                val stack: Stack<Set<String>> = Stack()
+                while (rest.isNotEmpty()) {
+                    stack.push(rest)
+                    rest = rest.flatMap { it.checkWith(rules, first) }.toSet()
+                }
+
+                while (stack.isNotEmpty()) {
+                    rest = stack.pop()
+                    repeat(stack.size + 1) { _ ->
+                        rest = rest.flatMap { it.checkWith(rules, second) }.toSet()
+                    }
+                    res += rest
+                }
+            }
+
+            else -> {
+                val options = rules[cur]!!
+                for (option in options) {
+                    var rest = setOf(this)
+                    for (rule in option) {
+                        if (rule.toIntOrNull() != null) {
+                            rest = rest.flatMap { it.checkWith(rules, rule.toInt()) }.toSet()
+                        } else {
+                            if (this.isNotEmpty() && this.first() == rule.first()) {
+                                return setOf(this.drop(1))
+                            } else {
+                                return setOf()
+                            }
+                        }
+                    }
+
+                    res += rest
+                }
+            }
+        }
+
+        return res
+    }
+
+    private fun parseInput(lines: ArrayList<String>): Pair<ArrayList<String>, ArrayList<String>> {
+        val rules = ArrayList<String>()
+        val messages = ArrayList<String>()
+
+        var i = 0
+        while (lines[i].isNotEmpty()) {
+            rules.add(lines[i])
+            i++
+        }
+        i++
+        while (i < lines.size) {
+            messages.add(lines[i])
+            i++
+        }
+
+        return Pair(rules, messages)
     }
 }

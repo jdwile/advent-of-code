d6c225fc21c0c7399e8516c195e23363523d9a3e
jdwile <jdwile@sep.com>
2021-11-30 17:22:34 -0500

    Revamp driver + format files

--- 2021/driver.py
+++ 2021/driver.py
@@ -1,6 +1,21 @@
 from day_01.solution import Solution
+from utils.aoc import timer
 
 day1 = Solution()
 
-print(f'Day 1, Part 1: {day1.solve_part_one()}')
-print(f'Day 1, Part 2: {day1.solve_part_two()}')
+days = [day1, day1, day1]
+
+
+@timer
+def run_part_one(day: int):
+    print(f"Day {day+1}, Part 1: {days[day].solve_part_one()}")
+
+
+@timer
+def run_part_two(day: int):
+    print(f"Day {day+1}, Part 2: {days[day].solve_part_two()}")
+
+
+for day in range(len(days)):
+    run_part_one(day)
+    run_part_two(day)
--- 2021/utils/aoc.py
+++ 2021/utils/aoc.py
@@ -1,16 +1,30 @@
 from typing import List
+import time
 
-def input_as_string(filename:str) -> str:
+
+def input_as_string(filename: str) -> str:
     """returns the content of the input file as a string"""
     with open(filename) as f:
         return f.read().rstrip("\n")
 
-def input_as_lines(filename:str) -> List[str]:
+
+def input_as_lines(filename: str) -> List[str]:
     """Return a list where each line in the input file is an element of the list"""
     return input_as_string(filename).split("\n")
 
-def input_as_ints(filename:str) -> List[int]:
+
+def input_as_ints(filename: str) -> List[int]:
     """Return a list where each line in the input file is an element of the list, converted into an integer"""
     lines = input_as_lines(filename)
-    line_as_int = lambda l: int(l.rstrip('\n'))
+    line_as_int = lambda l: int(l.rstrip("\n"))
     return list(map(line_as_int, lines))
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        print(f"\nTime required: {(time.time() - start_time)*1000:.2f} ms\n")
+        return result
+
+    return wrapper

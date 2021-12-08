1bbe4e8694035ecfca48086683614a1e8d9b3e7b
jdwile <jdwile@sep.com>
2021-12-08 12:33:30 -0500

    clean up part 2 with pattern matching

--- 2021/day_08/solution.py
+++ 2021/day_08/solution.py
@@ -8,6 +8,7 @@ class Solution:
         filename="C:\\Users\\Jenner\\bench\\advent-of-code\\2021\\day_08\\input.txt",
     ) -> None:
         self.signals, self.outputs = self.parse_signals(input_as_lines(filename))
+        self.known_signals = {}
 
     def solve_part_one(self) -> str:
         relevant_counts = [2, 3, 4, 7]
@@ -19,72 +20,39 @@ class Solution:
         )
 
     def solve_part_two(self) -> str:
-        relevant_counts = {2: 1, 3: 7, 4: 4, 7: 8}
-        known_signals = {}
         result = 0
         for i in range(len(self.signals)):
             signal_list = self.signals[i]
             output_list = self.outputs[i]
-            uniques = [
-                signal
-                for signal in signal_list
-                if len(signal) in relevant_counts.keys()
-            ]
 
-            for unique in uniques:
-                known_signals[relevant_counts[len(unique)]] = unique
+            for signal in signal_list:
+                match len(signal):
+                    case 2: self.known_signals[1] = signal
+                    case 4: self.known_signals[4] = signal
+                    case 3: self.known_signals[7] = signal
+                    case 7: self.known_signals[8] = signal
 
-            remaining_signals = [
-                signal for signal in signal_list if signal not in uniques
-            ]
+            for signal in signal_list:
+                match len(signal), self.common_with(1, signal), self.common_with(4, signal):
+                    case 6, 2, 3: self.known_signals[0] = signal
+                    case 5, 1, 2: self.known_signals[2] = signal
+                    case 5, 2, _: self.known_signals[3] = signal
+                    case 5, 1, 3: self.known_signals[5] = signal
+                    case 6, 1, 3: self.known_signals[6] = signal
+                    case 6, 2, 4: self.known_signals[9] = signal
 
-            known_signals[9] = [
-                signal
-                for signal in remaining_signals
-                if all([c in signal for c in known_signals[4]])
-            ][0]
-            remaining_signals.remove(known_signals[9])
-
-            five_counts = [signal for signal in remaining_signals if len(signal) == 5]
-            known_signals[3] = [
-                signal
-                for signal in five_counts
-                if all([c in signal for c in known_signals[1]])
-            ][0]
-            five_counts.remove(known_signals[3])
-            remaining_signals.remove(known_signals[3])
-
-            known_signals[5] = [
-                signal
-                for signal in five_counts
-                if len([c for c in signal if c in known_signals[4]]) == 3
-            ][0]
-            five_counts.remove(known_signals[5])
-            remaining_signals.remove(known_signals[5])
-
-            known_signals[2] = five_counts[0]
-            remaining_signals.remove(known_signals[2])
-
-            known_signals[0] = [
-                signal
-                for signal in remaining_signals
-                if all([c in signal for c in known_signals[1]])
-            ][0]
-
-            remaining_signals.remove(known_signals[0])
-            known_signals[6] = remaining_signals[0]
-
-            for k in known_signals.keys():
-                known_signals[k] = "".join(sorted(known_signals[k]))
-            decode = {v: k for k, v in known_signals.items()}
+            decode = {"".join(sorted(v)): str(k) for k, v in self.known_signals.items()}
 
             output = ""
             for o in output_list:
-                output += str(decode["".join(sorted(o))])
+                output += decode["".join(sorted(o))]
 
             result += int(output)
 
         return result
+
+    def common_with(self, num, pattern) -> int:
+        return len([c for c in pattern if c in self.known_signals[num]])
 
     def parse_signals(self, lines) -> tuple:
         signals, outputs = [], []

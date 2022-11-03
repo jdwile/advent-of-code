8762b655d82f4f226b3b560b770ab601b74df0bb
jdwile <jdwile@sep.com>
2022-11-02 21:40:36 -0400

    working pizza prototype

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,31 @@
-const world = 'world';
+#! /usr/bin/env node
 
-export function hello(who: string = world): string {
-    return `Hello ${who}! `;
-}
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const path = require('path');
+const program = require('commander');
+
+
+clear();
+console.log(
+    chalk.red(
+        figlet.textSync('aoc-cli', { horizontalLayout: 'full' })
+    )
+);
+
+program
+    .version('0.0.1')
+    .description("An example CLI for ordering pizza's")
+    .option('-p, --peppers', 'Add peppers')
+    .option('-P, --pineapple', 'Add pineapple')
+    .option('-b, --bbq', 'Add bbq sauce')
+    .option('-c, --cheese <type>', 'Add the specified type of cheese [marble]')
+    .option('-C, --no-cheese', 'You do not want any cheese')
+    .parse();
+
+console.log('you ordered a pizza with:'); if (program.peppers) console.log('  - peppers');
+console.log(program.opts());
+if (program.pineapple) console.log('  - pineapple');
+if (program.bbq) console.log('  - bbq');
+// console.log(program);

c0b2caeb95d204ee070089a0802d37661a87016c
jdwile <jdwile@sep.com>
2022-11-19 10:49:32 -0500

    Handle NotImplemented errors differently in runner

--- 2022/src/index.ts
+++ 2022/src/index.ts
@@ -1,10 +1,15 @@
 import * as solutions from './solutions';
+import { NotImplementedError } from './solutions/errors';
 
 export async function execute() {
     console.log(solutions);
     Object.keys(solutions).forEach(async (className) => {
         const sol = new solutions[className]();
-        await sol.solve();
+        await sol.solve()
+            .catch((error: Error) => {
+                if (!(error instanceof NotImplementedError)) throw error;
+                console.log(error.message);
+            });
     })
 }
 
---
+++ 2022/src/solutions/errors.ts
@@ -0,0 +1,7 @@
+export class NotImplementedError extends Error {
+    constructor() {
+        super('Not implemented');
+
+        Object.setPrototypeOf(this, NotImplementedError.prototype);
+    }
+}

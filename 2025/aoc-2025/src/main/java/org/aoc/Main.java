c2c67be9e69720294ba08c651914e6af09e13bb3
jdwile <jwile14@gmail.com>
2025-12-05 10:38:31 -0500

    Day 5 done

--- 2025/aoc-2025/src/main/java/org/aoc/Main.java
+++ 2025/aoc-2025/src/main/java/org/aoc/Main.java
@@ -1,11 +1,11 @@
 package org.aoc;
 
 import org.aoc.solutions.Solution;
-import org.aoc.solutions.days.day4.Day4;
+import org.aoc.solutions.days.day5.Day5;
 
 public class Main {
   public static void main(String[] args) {
-    Solution sol = new Day4();
+    Solution sol = new Day5();
     System.out.println("Part 1: " + sol.partOne());
     System.out.println("Part 2: " + sol.partTwo());
   }
---
+++ 2025/aoc-2025/src/main/java/org/aoc/solutions/days/day5/Day5.java
@@ -0,0 +1,117 @@
+package org.aoc.solutions.days.day5;
+
+import com.google.common.collect.Range;
+import org.aoc.solutions.AbstractSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day5 extends AbstractSolution {
+  private final List<String> input;
+  private final List<Range<Long>> ranges;
+  private final List<Long> ingredients;
+
+  public Day5() {
+    this.input = this.readFromFile("day5/input.txt");
+    this.ranges = compressRanges(parseRanges(input));
+    this.ingredients = parseIngredients(input);
+  }
+
+  public Day5(List<String> input) {
+    this.input = input;
+    this.ranges = compressRanges(parseRanges(input));
+    this.ingredients = parseIngredients(input);
+  }
+
+  @Override
+  public String partOne() {
+    return "" + ingredients.stream()
+      .filter(this::isFresh)
+      .toList()
+      .size();
+  }
+
+  @Override
+  public String partTwo() {
+    return "" + ranges
+      .stream()
+      .map(range -> range.upperEndpoint() - range.lowerEndpoint() + 1)
+      .mapToLong(Long::longValue)
+      .sum();
+  }
+
+  private List<Long> parseIngredients(List<String> input) {
+    List<Long> ingredients = new ArrayList<>();
+
+    for (String line : input) {
+      if (line.contains("-") || line.isEmpty()) continue;
+
+      ingredients.add(Long.parseLong(line));
+    }
+
+    return ingredients;
+  }
+
+  private List<Range<Long>> parseRanges(List<String> input) {
+    Pattern rangePattern = Pattern.compile("^(\\d+)-(\\d+)$");
+    List<Range<Long>> ranges = new ArrayList<>();
+
+    for (String line : input) {
+      if (line.isEmpty()) break;
+
+      Matcher m = rangePattern.matcher(line);
+      if (m.matches()) {
+        Long low = Long.parseLong(m.group(1));
+        Long high = Long.parseLong(m.group(2));
+        ranges.add(Range.closed(low, high));
+      }
+    }
+
+    return ranges;
+  }
+
+  private List<Range<Long>> compressRanges(List<Range<Long>> ranges) {
+    List<Range<Long>> minimalRanges = new ArrayList<>(ranges);
+    boolean modified = true;
+
+    while (modified) {
+      modified = false;
+
+      for (int i = 0; i < minimalRanges.size() - 1; i++) {
+        Range<Long> curRange = minimalRanges.get(i);
+
+        for (int j = i + 1; j < minimalRanges.size(); j++) {
+          Range<Long> existing = minimalRanges.get(j);
+
+          if (existing.encloses(curRange)) {
+            minimalRanges.remove(curRange);
+            modified = true;
+            break;
+          }
+
+          if (existing.isConnected(curRange)) {
+            Range<Long> span = existing.span(curRange);
+            if (span.encloses(curRange) || span.encloses(existing)) {
+              minimalRanges.remove(existing);
+              minimalRanges.remove(curRange);
+              minimalRanges.add(span);
+              modified = true;
+              break;
+            }
+          }
+        }
+        if (modified) break;
+      }
+    }
+    return minimalRanges;
+  }
+
+  private boolean isFresh(Long ingredient) {
+    for (Range<Long> range : ranges) {
+      if (range.contains(ingredient)) return true;
+    }
+    return false;
+  }
+}
